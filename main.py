cb4ab505bb2194a61659a629972eea616934be8a
Andrew Foster <and@rew.io>
2018-04-23 03:23:34 +0100

    Added lookup tagger code, plotted it and generated confusion matrix.
    Also added the code for the unigram tagger for debugging the confusion matrix code. The code in main.py is beginning to get a bit unbearable so it will be refactored soon, the repetition is killing me.

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ for category in brown.categories():
 average_accuracies.append(np.average(accuracies))
 
 # plot the accuracy of the taggers and their accuracies thus far on a bar chart.
-plot_bar(taggers_used, average_accuracies, "default-only")
+plot_bar(taggers_used, average_accuracies, "default")
 
 # print a pretty-printed confusion matrix for the default tagger against the news category of the brown corpus.
 print("\n" + taggers.create_confusion_matrix(brown, "news", default_tagger))
@@ -49,3 +49,47 @@ for category in brown.categories():
 average_accuracies.append(np.average(accuracies))
 plot_bar(taggers_used, average_accuracies, "regex")
 print("\n" + taggers.create_confusion_matrix(brown, "news", regex_tagger))
+
+# ============================================================
+# Lookup Tagger:
+# ============================================================
+
+# instantiate our containers.
+all_words        = []
+all_tagged_words = []
+
+# iterate over the brown corpus tracking words and tagged words, to train our lookup tagger.
+for category in brown.categories():
+    for word in brown.words(categories=category):
+        all_words.append(word)
+    for tagged_word in brown.tagged_words(categories=category):
+        all_tagged_words.append(tagged_word)
+        
+# same as the previous two code blocks.    
+    
+taggers_used.append("Lookup")        
+lookup_tagger = taggers.lookup_tagger(all_words, all_tagged_words, default_tagger)
+accuracies = []
+for category in brown.categories():
+    accuracies.append(lookup_tagger.evaluate(brown.tagged_sents(categories=category)))
+average_accuracies.append(np.average(accuracies))
+plot_bar(taggers_used, average_accuracies, "lookup")
+print("\n" + taggers.create_confusion_matrix(brown, "news", lookup_tagger))
+
+# ============================================================
+# Unigram Tagger:
+# ============================================================
+
+all_tagged_sentences = []
+for category in brown.categories():
+    for sentence in brown.tagged_sents(categories=category):
+        all_tagged_sentences.append(sentence)
+
+taggers_used.append("Unigram")  
+size = int(len(all_tagged_sentences) * 0.8)
+unigram_tagger = taggers.unigram_tagger(all_tagged_sentences[:size], default_tagger)
+accuracies = []
+for category in brown.categories():
+    accuracies.append(unigram_tagger.evaluate(brown.tagged_sents(categories=category)))
+plot_bar(taggers_used, average_accuracies, "lookup")
+print("\n" + taggers.create_confusion_matrix(brown, "news", unigram_tagger))
